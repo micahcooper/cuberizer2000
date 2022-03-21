0f75f486a3d38e218f855e0ce9525d0ce0821fcc
Micah R Cooper <mrcooper@uga.edu>
2022-03-21 19:26:57 -0400

    adds 54 colors

--- testDriver.py
+++ testDriver.py
@@ -7,7 +7,7 @@ print('')
 print('=====TEST ONE=====')
 
 ######test case 1######
-testcase1 = list(("r", "g", "w"))
+testcase1 = list(("r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b","r","g","b","w","w","b"))
 cube1 = cube.Cube(testcase1)
 for x in range(3):
     print('run: check position ', x, cube1.checkColorAT(x))
